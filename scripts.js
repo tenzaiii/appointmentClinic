9120853e80c8ea1381377b8522fdf370e7d4d475
tenz_aiii <101763751+tenzaiii@users.noreply.github.com>
2025-11-10 00:23:07 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+ var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      rewind: true,
+
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
